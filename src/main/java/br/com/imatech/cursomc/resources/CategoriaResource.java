c73fe572b27aae34b186e2e205a74e2c73c2207f
Marlos Oliveira <marlos.iguatu@gmail.com>
2020-06-30 14:46:32 -0300

    Deletando uma categoria com DELETE

--- src/main/java/br/com/imatech/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/imatech/cursomc/resources/CategoriaResource.java
@@ -40,4 +40,13 @@ public class CategoriaResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+		
+	}
+	
 }
