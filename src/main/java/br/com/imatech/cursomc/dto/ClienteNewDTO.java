452c44173c23bf4da7662a8e2d3217b29b10685c
Marlos Oliveira <marlos.iguatu@gmail.com>
2020-07-05 16:39:32 -0300

    Validacao customizada de CPF ou CNPJ na insertao de cliente

--- src/main/java/br/com/imatech/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/br/com/imatech/cursomc/dto/ClienteNewDTO.java
@@ -2,20 +2,32 @@ package br.com.imatech.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+import br.com.imatech.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Length(min=5, max=120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Length(min=5, max=120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String email;
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String cpfOuCnpj;
 	private Integer tipo;
-
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String logradouro;
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String cep;
-
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
